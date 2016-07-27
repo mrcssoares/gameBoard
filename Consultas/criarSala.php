c6d90ab751687d6116125687ab13d33e92a29716
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2016-07-27 14:36:41 -0400

    funcionalidades criar e entrar em Sala em php

---
+++ Consultas/criarSala.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcos
+ * Date: 27/07/16
+ * Time: 14:30
+ */
+  $id = "NULL";
+  $nome  = $_GET['nome'];
+
+  try{
+      #Conexão com MySQL via PDO_MYSQL
+      $DBH = new PDO("mysql:host=localhost;dbname=gameBoard", "root", "");
+  }catch (PDOException $e) {
+      echo $e->getMessage();
+  }
+
+  $sql = "select * from SALA where Nome like '".$nome."'";
+  $resultado = $DBH->query($sql);// or die ("Error: ".$resultado->erroInfo());
+
+  $sql = $resultado->fetch(PDO::FETCH_OBJ);
+  $count = $resultado->rowCount();
+  echo $count;
+
+  if($count == 0){
+      $resultado = $DBH->prepare("INSERT INTO SALA (ID, Nome) VALUES (NULL, '$nome')");
+      $resultado->execute();
+  }
+?>
---
+++ Consultas/entrarSala.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcos
+ * Date: 27/07/16
+ * Time: 14:31
+ */
+  $ID = "NULL";
+  $nome =$_GET['nome'];
+  $sala = $_GET['sala'];
+
+
+  try{
+      #Conexão com MySQL via PDO_MYSQL
+      $DBH = new PDO("mysql:host=localhost;dbname=gameBoard", "root", "");
+  }catch (PDOException $e) {
+      echo $e->getMessage();
+  }
+
+  //retorna id do time ja cadastrado posteriormente\\
+  $sql_id_time = $DBH->query("SELECT ID FROM SALA WHERE Nome = '".$sala."'") or die ("Erro: ".$sql_id_time->erroInfo());
+  $id = $sql_id_time->fetch(PDO::FETCH_OBJ);
+  $nt = $id->ID;
+
+  $consulta = $DBH->query("SELECT IdSala FROM JOGADOR WHERE Nome = '$nome' AND (IdSala = '$nt')");
+  $count = $consulta->rowCount();
+
+  // se jogador não existe, ele é cadastrado
+  if ($count == 0){
+      $insercao = $DBH->prepare("INSERT INTO JOGADOR (ID, Nome,Fase, IdSala) VALUES ('$ID', '$nome', '0', '$nt')") or die ("Error: " .$insercao->erroInfo());
+      $insercao->execute();
+      echo"Jogador entrou na Sala: ".$sala;//true
+  }else
+      echo "Jogador ja existe."//false
+?>
