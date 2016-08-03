edc9ff42ca65ff0344b3b5750c817f7d71e3b234
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2016-08-03 13:14:00 -0400

    entrar sala listView e google-services

--- GameBoardQuestion/app/src/main/java/com/example/marcos/gameboardquestion/Entrar_em_sala_Activity.java
+++ GameBoardQuestion/app/src/main/java/com/example/marcos/gameboardquestion/Entrar_em_sala_Activity.java
@@ -2,7 +2,9 @@ package com.example.marcos.gameboardquestion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
@@ -19,5 +21,14 @@ public class  Entrar_em_sala_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrar_em_sala_);
+
+        final String[] salas = new String[10];
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, salas);
+        final ListView listView_salas = (ListView)findViewById(R.id.listView_lista_de_salas);
+
+
+
     }
+
+
 }
