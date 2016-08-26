ebb4cbedae417bc81250e8c5de0091b1feeb5c97
Gisele Cristina <gcmn@icomp.ufam.edu.br>
2016-08-26 16:03:58 -0400

    title bar ocultada

--- GameBoardQuestion/app/src/main/java/com/example/marcos/gameboardquestion/SplashActivity.java
+++ GameBoardQuestion/app/src/main/java/com/example/marcos/gameboardquestion/SplashActivity.java
@@ -1,16 +1,19 @@
 package com.example.marcos.gameboardquestion;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
     private static int TEMPO_SPLASH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
